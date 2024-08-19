07ca973fe1252eca7e2d5dab565aa12c776841a0
kimu805 <kimukimuok805@gmail.com>
2024-08-19 14:54:25 +0900

    change position of devise strong parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,3 @@
 class UsersController < ApplicationController
-  before_action :configure_permitted_parameter, if: :devise_controller?
 
-  private
-  def configure_permitted_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname, :firstname, :birthday])
-  end
 end
